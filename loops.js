436dc9c4247391e43d2118679da6613faede8e0a
Phill Schrader <blackholekingdom@yahoo.com>
2017-09-19 19:11:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+debugger()
+
+function forLoop(array) {
+  for (let i = 0; i < 25, i++) {
+    if (i = 1) {
+      array = [...array, "I am 1 strange loop."];
+    } else {
+      array = [...array, `I am ${i} strange loops.`];
+    }
+  }
+  return array
+};
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(--n)
+  };
+  if (n == 0) {
+    return 'done'
+  };
+};
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  };
+  do {
+    array = array.slice(0, array.length - 1)
+    } while (array.length > 0 && maybeTrue())
+  return array
+}
